529eb46a70e1a2ab81160ca7d823dc0ab969c6b0
DiegoC210 <diegorcuevas1@gmail.com>
2026-05-11 14:42:49 -0500

    Update BSTGame.h

--- BSTGame.h
+++ BSTGame.h
@@ -1,1 +1,36 @@
+#ifndef BSTGAME_H
+#define BSTGAME_H
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class BSTGame {
+private:
+
+  class Node {
+  public:
+      string question;
+      int points
+      string correctAnswer;
+
+      Node* left
+      Node* right
+      Node(string q, int p, string ans);
+};
+
+Node* Roo;
+
+void deleteTree(Node* node):
+int maxPoints(Node* node);
+
+public:
+  BSTGame();
+  ~BSTGame();
+
+  void game();
+  int maxPoints();
+};
+
+#endif
